b9f576e2bc024f77ffd0e553cc48ed5d50cf1fbe
Nishant <nishant.e.ekka@gmail.com>
2025-01-21 02:46:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+import pyodbc
+
+
+app = Flask(__name__)
+
+server = "moduleexercise.database.windows.net"
+database = 'StudentDB'
+username = 'testuser'
+password = 'Nishant@1234'
+
+conn = pyodbc.connect(
+    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+    f'SERVER={server};'
+    f'DATABASE={database};'
+    f'UID={username};'
+    f'PWD={password}'
+)
+
+cursor = conn.cursor()
+
+@app.route('/')
+def home():
+    return 'Hello, This is the Flask app intergrated with azure sql DB!'
+
+@app.route('/students', methods=['GET'])
+def get_studets():
+    cursor.execute("SELECT * FROM students")
+    students = [{'id': row[0], 'name': row[1], 'age': row[2]} for row in cursor.fetchall()]
+    return jsonify(students)
+
+@app.route('/students', methods=['POST'])
+def add_student():
+    data = request.get_json()
+    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?);", data['name'], data['age'])
+    conn.commit()
+    return jsonify({"message":"student successfully added"}), 201
+
+
+@app.route('/students/<int:id>', methods=['PUT'])
+def update_student(id):
+    data = request.get_json()
+    cursor.execute("UPDATE students SET name=?, age=? WHERE id=?", data['name'], data['age'], id)
+    conn.commit()
+    return jsonify({"message":f"student with id {id} successfully updated"}), 200
+
+@app.route('/students/<int:id>', methods=['DELETE'])
+def delete_student(id):
+    cursor.execute("DELETE FROM students WHERE id=?", id)
+    conn.commit()
+    return jsonify({"message":f"student with id {id} successfully deleted"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
